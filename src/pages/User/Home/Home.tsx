284ea34c44a8f8cbe46b24d792e2eca8c8999d59
= <2411550401@fema.edu.br>
2024-11-07 20:54:14 -0300

    feat: add fallback image at post

--- src/pages/User/Home/Home.tsx
+++ src/pages/User/Home/Home.tsx
@@ -16,7 +16,15 @@ import { Button } from "@/components/ui/button";
 
 const postService = new PostService();
 
+import image1 from "../../../Assets/image1.png";
+import image2 from "../../../Assets/image2.png";
+import image3 from "../../../Assets/image3.png";
+import image4 from "../../../Assets/image4.png";
+import image5 from "../../../Assets/image5.png";
+import fallback from "../../../Assets/fallback.png";
+
 const Home = () => {
+  const images = [image1, image2, image3, image4, image5];
   const [posts, setPosts] = useState<PostInterface[]>([]);
 
   useEffect(() => {
@@ -33,14 +41,28 @@ const Home = () => {
       <h1 className="text-slate-800 text-3xl font-bold">Novidades</h1>
       <section className="py-4 sm:columns-2 gap-4 space-y-4">
         {posts.length > 0 ? (
-          posts.map((post) => (
+          posts.map((post, i) => (
             <Card className="break-inside-avoid" key={post.id}>
               <CardHeader>
                 <CardTitle>{post.title}</CardTitle>
                 <CardDescription>{post.description}</CardDescription>
               </CardHeader>
               <CardContent>
-                <div className="w-full h-[300px] rounded-lg"></div>
+                {i < 5 ? (
+                  <div
+                    className="w-full h-[300px] rounded-lg"
+                    style={{
+                      background: `url(${images[i]}) no-repeat center center / cover`,
+                    }}
+                  ></div>
+                ) : (
+                  <div
+                    className="w-full h-[300px] rounded-lg"
+                    style={{
+                      background: `url(${fallback}) no-repeat center center / cover`,
+                    }}
+                  ></div>
+                )}
               </CardContent>
               {post.link && (
                 <CardFooter>
